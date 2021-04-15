5cf593a2e9646ac34c4d8530453d5795d58a122e
yanyun <596310898@163.com>
2021-04-15 15:12:28 +0800

    update

--- EACMscript/es6-es7/promise.js
+++ EACMscript/es6-es7/promise.js
@@ -1,14 +1,14 @@
-console.log('script start');                    // 1 代码开始执行
+console.log('script start');              
 setTimeout(function () {
-    console.log('settimeout')                               // 6 执行定时队列
+    console.log('settimeout')                          
 });
 let promise1 = new Promise(function (resolve) {
-    console.log('promise1')                     // 2 promise同步执行
+    console.log('promise1')                  
     resolve()
-    console.log('promise1 end')                 // 3 promise同步执行
+    console.log('promise1 end')               
 }).then(function () { 
-    console.log('promise2')                         // 5 同步代码执行结束 执行异步队列代码
+    console.log('promise2')                     
 });
-console.log('script end');                      // 4 代码同步执行结束
+console.log('script end');                      
 
 

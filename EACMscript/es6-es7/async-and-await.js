d6571f6a2caf68bcd0b83e3418fda25a2ba722d3
yanyun <596310898@163.com>
2021-06-06 18:35:00 +0800

    promise learn

--- EACMscript/es6-es7/async-and-await.js
+++ EACMscript/es6-es7/async-and-await.js
@@ -1,23 +1,39 @@
-async function asy1() {
-    const res = await new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (Math.random() * 10 > 5) {
-                resolve('定时器执行了');
-            } else {
-                reject(new Error('定时器错误'));
-            }
-        }, 1000);
-    });
-    return res;
+function f1() {
+    const p1 =  new  Promise((resolve, reject) => {
+        if(Math.random() * 10 > 5){
+            resolve()
+        } else {
+            reject()
+        }
+    })
+    const p2 = p1.then(function(){
+        console.log('resolved')
+    })
+    const p3 = p2.catch(function(){
+        console.log('rejected')
+    })
+    return p3
 }
 
-async function asy2() {
+
+async function f2() {
     try {
-        const res = await asy1();
-        console.log('获取返回值', res);
+        const res = await f1();
+        console.log('f2 resolve')
     } catch (err) {
-        console.warn('捕获异常：', err);
+        console.log('f2 rejected')
+    }
+    return Promise.reject();
+}
+
+
+async function f3() {
+    try {
+        const res = await f2();
+        console.log('f3 resolve')
+    } catch (err) {
+        console.log('f3 rejected')
     }
 }
 
-asy2();
+f3()

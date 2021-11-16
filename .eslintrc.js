b6d1d621e9336e83cdb4dc034eee36b9c24ece40
yanyun <596310898@163.com>
2021-11-16 10:59:20 +0800

    configuration file modification of vscode and eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,14 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
-    node: true,
-    es6: true,
+    node: true
   },
   extends: [
-    'plugin:vue/essential', // 必须遵守的
-    // 'plugin:vue/strongly-recommended', // 强烈推荐的
-    // 'plugin:vue/recommended', // 推荐的
-    '@vue/airbnb', // airbnb风格规范
+    "plugin:vue/essential"
+    // "@vue/prettier"
   ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  ignorePatterns: ["**/dist/**/*.js", "**/.vscode/*.js"],
   rules: {
-    'linebreak-style': 'off',
-    'max-len': ['error', 200],
-    'no-console': 'off',
-    'no-debugger': 'off',
-  },
-};
+    "no-console": "off",
+    "no-unused-vars": "error"
+  }
+}

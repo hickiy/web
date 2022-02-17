10eba23a5ba0b9ea5216e8ee07cec22cc53eacbb
yanyun <596310898@163.com>
2022-02-17 17:42:39 +0800

    learn vue extend method and js extantion mode

--- EACMscript/es6-es7/extansion.js
+++ EACMscript/es6-es7/extansion.js
@@ -1,49 +1,61 @@
-/**
- * 在js的继承中，实例的__proto__属性总是指向实例的构造函数的prototype对象
- * 在es5中所有的函数对象__proto__属性总是指向Function的prototype对象，包括Function自身
- * 在es6中基类依然遵循这个规则，子类的__proto__属性则指向基类，这相当于子类继承了基类的静态属性
- * 在es6的继承中，子类作为构造函数时，子类的构造器的原型对象是基类的实例。子类作为对象时，子类的__proto__属性是父类
- */
-class Parent {
-  constructor() {
-    console.log('is Parent class constructor here');
-    console.log(this);
-    console.log(new.target.name);
-  }
-
-  static staticMethod() {
-    console.log('is Parent class staticMethod here');
-    console.log(this);
-  }
-
-  method() {
-    console.log('is Parent class method here');
-    console.log(this);
-  }
+// 超类
+function Super(name) {
+  this.name = name;
+}
+// 超类方法
+Super.prototype.sayName = function () {
+  console.log(this.name);
+}
+Super.prototype.sayAge = function () {
+  console.log(this.age);
 }
 
-class Children extends Parent {
-  constructor() {
-    super(); // 必须调用super函数，相当封装了于es5中组合继承模式的执行过程，如果传参则相当于借用父类构造器添加属性，此时父类构造器中的this指向子类的实例, new.target指向子类
-    // super.staticMethod(); // super对象，相当于引用的是父类的构造器的原型对象,父类的静态方法staticMethod定义在父类构造器上，这里会报错 staticMethod is not a function
-    super.method(); // super对象，相当于引用的是父类的构造器的原型对象 父类的继承方法是定义在父类构造器的原型对象上，这里可以正常调用父类的继承方法method，父类的继承方法中this指向子类实例
-    console.log('Children class construct:', this); // 子类构造器中的this指向子类实例
-  }
-
-  static staticMethod() {
-    // super(); // 子类的静态方法中无法引用到super函数, 会报语法错误 keyword unexpected here
-    super.staticMethod(); // 在子类的静态方法中super对象，指向父类构造器, 这里可以正常调用父类的静态方法staticMethod，父类的静态方法中this指向子类
-    // super.method(); // 在子类的静态方法中super对象，指向父类构造器，继承方法method是定义在父类的原型中，这里调用会报错 method is not a function
-    console.log('Children class staticMethod', this); // 子类的的静态方法中this指向子类
-  }
-
-  method() {
-    // super(); // 子类的继承方法中无法引用到super函数, 会报语法错误 keyword unexpected here
-    // super.staticMethod(); // 在子类的继承方法中super对象，指向父类构造器的原型, 父类staticMethod定义在父类构造器上，这里会报错 staticMethod is not a function
-    super.method(); // 在子类的继承方法中super对象，指向父类的构造器的原型, 这里可以正常调用父类的继承方法method，父类的继承方法中this指向子类实例
-    console.log('Children class method', this); // 子类的继承方法中的this指向子类实例
-  }
+// 基于原型继承父类方法
+function Sub(name) {
+  this.name = name;
 }
-// Children.staticMethod();
-const instance = new Children();
-instance.method();
+Sub.prototype = new Super();
+Sub.prototype.constractor = Sub
+var person = new Sub('原型继承父类方法sayName');
+person.sayName();
+
+
+// 借用构造函数继承父类属性
+function Sbu2(name) {
+  Super.call(this, name);
+}
+Sbu2.prototype.sayName = function () {
+  console.log(this.name);
+}
+var person2 = new Sbu2('借用构造函数添加name属性');
+person2.sayName();
+
+// 组合继承
+function Sbu3(name) {
+  Super.call(this, name);
+}
+
+Sbu3.prototype = new Super();
+Sbu3.prototype.constractor = Sbu3;
+var person3 = new Sbu3('同时利用原型和借用构造函数模式，实现属性与方法的继承');
+person3.sayName();
+
+// 组合继承优化版本
+function Sub4(name) {
+  Super.call(this, name);
+}
+
+Sub4.prototype = Object.create(Super.prototype);
+Sub4.prototype.constractor = Sub4;
+var person4 = new Sub4('组合继承优化')
+person4.sayName();
+
+// 寄生继承（实际是利用函数增强Super超类的实例）
+function Sub5(name, age) {
+  var person = new Super(name);
+  person.age = age;
+  return person;
+}
+var person5 = new Sub5('寄生继承输出父类定义的name属性', '寄生继承输出子类增强age属性');
+person5.sayName();
+person5.sayAge();

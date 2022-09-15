e13c295705803e69fe2cf25cc7ff994511d1e1cd
jinbo.cai <596310898@163.com>
2022-09-15 09:43:25 +0800

    go over destructuring

--- EACMscript/es6-es7/destructuring.js
+++ EACMscript/es6-es7/destructuring.js
@@ -3,9 +3,10 @@
  * 数组的解构赋值 按照索引位解构数组元素
  * 解构赋值 可以解构原型中的属性  可以解构不可枚举属性
  * rest操作符 只能展开对象自有可枚举属性
+ * 结构赋值支持重命名
  */
 const arr = ['elemente'];
-const obj = {};
+const obj = { a: 1, b: 2 };
 arr.testAttrOfArr = 'testAttrOfArr';
 Object.defineProperty(obj, 'testAttrOfObj', {
   value: 'testAttrOfObj',
@@ -17,8 +18,8 @@ const [element] = arr;
 global.console.log(toString);
 global.console.log(testAttrOfObj);
 global.console.log(testAttrOfArr, length, element, arr[0], arr['0']);
-const obj1 = { a: 1, b: 2 };
 
-const { a, b: b1, c = 3 } = obj1;
+const { a, b: b1, c = 3 } = obj;
 
 console.log(a, b1, c);
+

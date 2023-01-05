88276b14e1c6e3963ccc3429bca63eb084ae6610
jinbo.cai <596310898@163.com>
2023-01-05 18:33:12 +0800

    practice all Math static method

---
+++ html5/Math/math.js
@@ -0,0 +1,32 @@
+// console.log(Math.LN10)
+// console.log(Math.log(10));
+// console.log(Math.pow(Math.E, Math.log(10)));
+
+// console.log(Math.LN2);
+// console.log(Math.log(2));
+// console.log(Math.pow(Math.E, Math.log(2)));
+
+// console.log(Math.LOG10E)
+// console.log(Math.log10(Math.E))
+// console.log(Math.E);
+// console.log(Math.pow(10, Math.log10(Math.E)));
+
+// console.log(Math.LOG2E)
+// console.log(Math.log2(Math.E))
+// console.log(Math.E);
+// console.log(Math.pow(2, Math.log2(Math.E)))
+
+
+// console.log(Math.SQRT2);
+// console.log(Math.sqrt(2));
+
+// console.log(Math.SQRT1_2);
+// console.log(Math.sqrt(0.5));
+
+
+// console.log(Math.exp(-1));
+// console.log(Math.pow(Math.E, -1));
+// console.log(Math.exp(1));
+// console.log(Math.pow(Math.E, 1));
+// console.log(Math.exp(0));
+// console.log(Math.pow(Math.E, 0));

6bcacd1b5612c9fa0e10e6a07bfdf29d0d87a55a
hickey <596310898@163.com>
2023-04-13 14:12:01 +0800

    the character of the research function when useing the async and await keywork

--- EACMscript/es6-es7/async-and-await.js
+++ EACMscript/es6-es7/async-and-await.js
@@ -1,39 +1,13 @@
 function f1() {
-    const p1 =  new  Promise((resolve, reject) => {
-        if(Math.random() * 10 > 5){
-            resolve()
-        } else {
-            reject()
-        }
-    })
-    const p2 = p1.then(function(){
-        console.log('resolved')
-    })
-    const p3 = p2.catch(function(){
-        console.log('rejected')
-    })
-    return p3
+  return new Promise((resolve, reject) => {
+    reject('reject');
+  });
 }
-
 
 async function f2() {
-    try {
-        const res = await f1();
-        console.log('f2 resolve')
-    } catch (err) {
-        console.log('f2 rejected')
-    }
-    return Promise.reject();
+    return await f1();
 }
 
-
-async function f3() {
-    try {
-        const res = await f2();
-        console.log('f3 resolve')
-    } catch (err) {
-        console.log('f3 rejected')
-    }
-}
-
-f3()
+f2().catch(function (err) {
+  console.log(err);
+});

8bf6cf614a4ee2225b50501cb9f91f5675e3a214
hickey <596310898@163.com>
2023-05-10 11:37:56 +0800

    ts practice

---
+++ eacms-script/typescript/keyof.ts
@@ -0,0 +1,15 @@
+function pluck<T, S extends keyof T>(o: T, names: S[]): T[S][] {
+  return names.map(n => o[n]);
+}
+
+interface Person {
+    name: string;
+    age: number;
+    is: boolean;
+}
+let person: Person = {
+    name: 'Jarid',
+    age: 35,
+    is: true,
+};
+let strings: Array<number | string | boolean> = pluck(person, ['name', 'age', 'is']); // ok, string[]

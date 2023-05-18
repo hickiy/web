f881836ba3ea391784a12b45056994f3ba829e4c
hickey <596310898@163.com>
2023-05-18 15:24:26 +0800

    study the precision isuss of any number calculation

--- utils/number.js
+++ utils/number.js
@@ -1,7 +1,5 @@
-import BigNumber from 'bignumber.js';
 /**
- * 格式化模块
- * @description 各种格式化工具函数, 如时间格式化, 价格格式化等等
+ * @description 用于数字格式化
  */
 
 /**
@@ -9,39 +7,27 @@ import BigNumber from 'bignumber.js';
  * @param {number} originNumber     数字
  * @param {number} precision        数字精度
  */
-const formatNumber = (originNumber = 0, precision = 2) => {
-    if (Number.isNaN(Number(originNumber))) {
-        throw new Error('数字格式错误');
-    }
+function toInteger(number, precision) {
+  const num = Number(number);
+  if (Number.isNaN(num) || Number.isInteger(num)) {
+    return number;
+  }
+  const numStr = num.toFixed(precision);
+  const [int, float] = numStr.split('.');
+  return int * Math.pow(10, precision) + Number(float);
+}
 
-    const number = new BigNumber(Number(originNumber));
-    let numberString = number.toFixed();
+/**
+ * 货币风格的数字
+ * @param {number} val     数字
+ * @param {number} index   分隔位数
+ * @param {string} separated   分隔符
+ */
 
-    // 保留两位小数
-    const splitNumberString = numberString.split('.');
-    // 如果没有小数位, 则直接补齐精度位数的0
-    if (splitNumberString.length === 1) {
-        numberString = `${numberString}${precision === 0 ? '' : '.'}${'0'.repeat(precision)}`;
-    }
-    // 如果有小数位, 则补齐缺少的精度位数
-    else {
-        const integer = splitNumberString[0];
-        let decimal = splitNumberString[1];
-        if (decimal.length > precision) {
-            decimal = decimal.slice(0, precision);
-        } else {
-            decimal = `${decimal}${'0'.repeat(precision - decimal.length)}`;
-        }
-        numberString = `${integer}${precision === 0 ? '' : '.'}${decimal}`;
-    }
-    return numberString;
-};
-
-export default formatNumber;
+function moneyStyleNumber(val, index = 3, separated = ',') {
+  if (!val) return val;
+  const reg = new RegExp(`\\B(?=(?:\\d{${index}})+\\b)`, 'g');
+  return String(val).replace(reg, separated);
+}
 
 
-export const moneyStyleNumber = (val, index = 3, separated = ',') => {
-    if (!val) return val;
-    const reg = new RegExp(`\\B(?=(?:\\d{${index}})+\\b)`, 'g');
-    return String(val).replace(reg, separated);
-};

4c1346db39cbde3048d3298c2a0d79fc14f89cb7
hickey <596310898@163.com>
2023-05-22 14:52:15 +0800

    tests accets the file system in the weChat mini program and upload file

--- mini-program/we-chat/pages/index/index.js
+++ mini-program/we-chat/pages/index/index.js
@@ -6,5 +6,24 @@ Page({
         console.log(res);
       }
     })
+  },
+  bindtap() {
+    wx.chooseMessageFile({
+      count: 100,
+      success: (res) => {
+        console.log(res);
+        this.uploadFile(res.tempFiles[0]);
+      }
+    })
+  },
+  uploadFile(file) {
+    wx.uploadFile({
+      filePath: file.path,
+      name: file.name,
+      url: 'http://localhost:49000/file/upload',
+      success(res) {
+        console.log(res);
+      }
+    })
   }
 })

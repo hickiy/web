7e08b2ee2d38c8d8ff4348d185f7e002ffc8df8f
hickey <596310898@163.com>
2023-07-10 16:41:05 +0800

    config: vitepress build config options update

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress';
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: '/blog/',
+  outDir: '.vitepress/blog',
   title: 'hickey blog',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config

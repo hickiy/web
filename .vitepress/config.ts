cf7dabe1cd6ec58eee36a86cdf5e14e67efb4388
hickey <596310898@163.com>
2023-08-02 16:55:47 +0800

    deploy: use github pages

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vitepress';
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/',
-  outDir: 'dist',
+  base: '/learning-web/',
+  outDir: '.vitepress/dist',
   title: 'hickey blog',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config

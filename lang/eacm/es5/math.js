eae9505aa4726a787cb89d3404adaa42d1342351
hickey <596310898@163.com>
2023-08-31 11:22:42 +0800

    lang: learn math.sin method in js

--- lang/eacm/es5/math.js
+++ lang/eacm/es5/math.js
@@ -16,13 +16,11 @@
 // console.log(Math.E);
 // console.log(Math.pow(2, Math.log2(Math.E)))
 
-
 // console.log(Math.SQRT2);
 // console.log(Math.sqrt(2));
 
 // console.log(Math.SQRT1_2);
 // console.log(Math.sqrt(0.5));
-
 
 // console.log(Math.exp(-1));
 // console.log(Math.pow(Math.E, -1));
@@ -30,3 +28,12 @@
 // console.log(Math.pow(Math.E, 1));
 // console.log(Math.exp(0));
 // console.log(Math.pow(Math.E, 0));
+
+// const angle = 90;
+// const sin = Math.sin((angle * Math.PI) / 180);
+// const asin = Math.asin(sin) * (180 / Math.PI);
+// console.log(sin, asin);
+
+// const angle = 2;
+// const cos = Math.sinh(angle);
+// console.log( Math.E**angle, Math.E**-angle, cos);

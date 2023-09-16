3ae57a4bc6a8f0eedc48a795c7d0294a8f33a0a1
hickey <596310898@163.com>
2023-09-16 22:15:02 +0800

    eacm: calculate power of digit

--- lang/eacm/es5/math.js
+++ lang/eacm/es5/math.js
@@ -32,8 +32,28 @@
 // const angle = 90;
 // const sin = Math.sin((angle * Math.PI) / 180);
 // const asin = Math.asin(sin) * (180 / Math.PI);
-// console.log(sin, asin);
+// console.log(sin, asin); // 1 90
 
-// const angle = 2;
 // const cos = Math.sinh(angle);
-// console.log( Math.E**angle, Math.E**-angle, cos);
+// console.log(cos); // 3.6268604078470186
+
+// var a = 10;
+// console.log(a / 0); // Infinity
+// console.log(0 / a); // 0
+// console.log(a / -0); // -Infinity
+// console.log( a / 0 === a / -0); // false
+
+// var b = 10;
+// console.log(0 ** b); // 0
+// console.log(b ** 0); // 1
+// console.log(b ** -0); // 1
+// console.log(b ** 1); // 10
+// console.log(b ** -1); // 0.1
+// console.log(b ** 2); // 100
+// console.log(b ** -2); // 0.01
+
+var base = 16;
+var exponent = 4 ** -1;
+var four_root_of_sixteen = base ** exponent;
+console.log(four_root_of_sixteen); // 2
+console.log((four_root_of_sixteen ** 4)); // 16

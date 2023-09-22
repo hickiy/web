78f6707ba93df270611e843b15e1c621970a4f95
hickey <596310898@163.com>
2023-09-22 11:53:46 +0800

    prec: es5<definedProperty api practice>

--- lang/eacm/es5/defineproperty.js
+++ lang/eacm/es5/defineproperty.js
@@ -1,18 +1,30 @@
 var obj = {};
-Object.defineProperty(obj, 'reduce', {
-  writable: true,
-});
-const discriptor = Object.getOwnPropertyDescriptor( obj, 'reduce');
-console.log(discriptor);
+// Object.defineProperty(obj, 'reduce', {
+//   writable: true,
+// });
+// const discriptor = Object.getOwnPropertyDescriptor( obj, 'reduce');
+// console.log(discriptor);
 
-obj.reduce = 12;
-console.log(obj.reduce)
+// obj.reduce = 12;
+// console.log(obj.reduce)
+
+// Object.defineProperty(obj, 'reduce', {
+//   writable: false,
+// })
+
+// const discriptor1 = Object.getOwnPropertyDescriptor( obj, 'reduce');
+// console.log(discriptor1);
+
 
 Object.defineProperty(obj, 'reduce', {
-  writable: false,
+  configurable: true,
+  enumerable: true,
+  get() {
+    return obj._reduce;
+  },
+  set(value) {
+    console.log(value);
+    obj._reduce = value;
+  }
 })
-
-const discriptor1 = Object.getOwnPropertyDescriptor( obj, 'reduce');
-console.log(discriptor1);
-
-
+obj.reduce = '123'

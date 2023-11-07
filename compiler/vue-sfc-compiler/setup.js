7f63fade87d21536b5df05f65855d0186e23b96c
hickey <596310898@163.com>
2023-11-07 14:24:40 +0800

    compiler: learn vue sfc compiler

--- compiler/vue-sfc-compiler/setup.js
+++ compiler/vue-sfc-compiler/setup.js
@@ -1,46 +1,20 @@
-import {
-  toDisplayString as _toDisplayString,
-  createElementVNode as _createElementVNode,
-  Fragment as _Fragment,
-  openBlock as _openBlock,
-  createElementBlock as _createElementBlock
-} from 'vue';
-
-const _hoisted_1 = /*#__PURE__*/ _createElementVNode('div', null, '12312312', -1 /* HOISTED */);
+import { toDisplayString as _toDisplayString, openBlock as _openBlock, createElementBlock as _createElementBlock } from 'vue';
 
 export function render(_ctx, _cache) {
-  return (
-    _openBlock(),
-    _createElementBlock(
-      _Fragment,
-      null,
-      [_createElementVNode('div', null, _toDisplayString(_ctx.count), 1 /* TEXT */), _hoisted_1],
-      64 /* STABLE_FRAGMENT */
-    )
-  );
+  return _openBlock(), _createElementBlock('div', null, _toDisplayString(_ctx.count), 1 /* TEXT */);
 }
 
-import { defineComponent as _defineComponent } from 'vue';
 import { ref, h } from 'vue';
 
-export default /*#__PURE__*/ _defineComponent({
+export default {
   __name: 'setup',
   setup(__props, { expose: __expose }) {
     __expose();
 
     const count = ref(1);
 
-    // export default {
-    //   setup() {
-    //     return () => h('div', count.value);
-    //     // return {
-    //     //   count
-    //     // };
-    //   }
-    // };
-
-    const __returned__ = { count };
+    const __returned__ = { count, ref, h };
     Object.defineProperty(__returned__, '__isScriptSetup', { enumerable: false, value: true });
     return __returned__;
   }
-});
+};
--- compiler/vue-sfc-compiler/setup.vue
+++ compiler/vue-sfc-compiler/setup.vue
@@ -1,20 +1,20 @@
 <!-- hickey 2023 -->
 <template>
   <div>{{ count }}</div>
-  <div>12312312</div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { ref, h } from 'vue';
-
 const count = ref(1);
-
-// export default {
-//   setup() {
-//     return () => h('div', count.value);
-//     // return {
-//     //   count
-//     // };
-//   }
-// };
 </script>
+
+<!-- <script>
+export default {
+  setup() {
+    return () => h('div', count.value);
+    return {
+      count
+    };
+  }
+};
+</script> -->

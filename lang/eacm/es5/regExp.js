aea4d3bcb679a3c15b8c53eea48531620ce28f45
hickey <596310898@163.com>
2023-11-14 19:40:41 +0800

    lang: es5<learn the escape rules of regext>

--- lang/eacm/es5/regExp.js
+++ lang/eacm/es5/regExp.js
@@ -3,8 +3,14 @@
 // const reg = /(?<=\d{3,})\d(?=\d{4,})/g
 // console.log(text.replace(reg, '*'))
 
+// var search = '?code=IWsGtLqtOQ+py/xlqrmhWj4ORf0MbP7o+w5iJhDoEs33tEvaxSiBGugkrVVTQhRqIqGQmwT/Zoi66w8KmIhCxQ==&fdId=185ba3a8def8fa06f2dd585447ab5635&serverurl=https%3A%2F%2Fportaltest.ionrocking.com%2Ffile%2Fviewer&failUrl=https%3A%2F%2Fportaltest.ionrocking.com%2Flogin'
+// var code = search.replace(/^.*code=([^&]+).*$/ig, '$1');
+// var id = search.replace(/^.*fdId=([^&]+).*$/ig, '$1');
+// console.log(code, id);
 
-var search = '?code=IWsGtLqtOQ+py/xlqrmhWj4ORf0MbP7o+w5iJhDoEs33tEvaxSiBGugkrVVTQhRqIqGQmwT/Zoi66w8KmIhCxQ==&fdId=185ba3a8def8fa06f2dd585447ab5635&serverurl=https%3A%2F%2Fportaltest.ionrocking.com%2Ffile%2Fviewer&failUrl=https%3A%2F%2Fportaltest.ionrocking.com%2Flogin'
-var code = search.replace(/^.*code=([^&]+).*$/ig, '$1');
-var id = search.replace(/^.*fdId=([^&]+).*$/ig, '$1');
-console.log(code, id);
+const string = '\\';
+const escaped = string.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+console.log(escaped);
+console.log(/\\/);
+console.log(new RegExp(escaped));
+console.log(new RegExp('\\\\'))

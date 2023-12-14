4f7f9c9fa380dcb5f0b4eb3f02d0dbc533dc1e26
hickey <596310898@163.com>
2023-12-14 11:47:11 +0800

    lang: eacm<comprehend the strict model>

--- lang/eacm/es6-es7/strict.js
+++ lang/eacm/es6-es7/strict.js
@@ -1,11 +1,14 @@
-// "use strict"
+/**
+ * 严格模式是基于模块和函数的，他们只影响当前模块或函数
+ * 不会影响导入的模块或者导入当前模块的模块
+ */
+
 function global() {
+  'use strict';
   console.log(this);
 }
- global()
-
- const global1 = () => {
+function global1() {
   console.log(this);
- }
-
- global1();
+}
+global();
+global1();

c8771f43244aaa576662591698cb51298b756c6d
hickey <596310898@163.com>
2024-03-19 19:20:54 +0800

    rust: format and print <how to use the std::fmt macro with println in rust>

--- rust/comments/comments.rs
+++ rust/comments/comments.rs
@@ -1,3 +1,4 @@
+
 fn main() {
   // This is an example of a line comment.
   // There are two slashes at the beginning of the line.
@@ -24,6 +25,6 @@ fn main() {
   // You can manipulate expressions more easily with block comments
   // than with line comments. Try deleting the comment delimiters
   // to change the result:
-  let x = 5 + /* 90 + */ 5;
+  let x = 5 +  90 + 5;
   println!("Is `x` 10 or 100? x = {}", x);
 }

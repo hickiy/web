f1b2fd83542bf23e971ca51a4b06bf8f79175dd0
hickey <596310898@163.com>
2025-05-27 17:58:02 +0800

    adjust the construction of the project for the Rust language section

--- lang/rust/basic-syntax/eq.rs
+++ lang/rust/basic-syntax/eq.rs
@@ -3,4 +3,9 @@ fn main() {
   let b = f64::NAN;
   println!("{}", a == b); // false
   println!("{:?}", a.partial_cmp(&b)); // None
+
+  let c = 1;
+  let d = 2;
+  println!("{}", c == d); // false
+  println!("{:?}", c.partial_cmp(&d)); // Some(Less)
 }
---
+++ lang/rust/target/debug/deps/hello_world-6b4177d71c32fa7f.d
@@ -0,0 +1,5 @@
+E:\learn\web\lang\rust\target\debug\deps\libhello_world-6b4177d71c32fa7f.rmeta: src\main.rs
+
+E:\learn\web\lang\rust\target\debug\deps\hello_world-6b4177d71c32fa7f.d: src\main.rs
+
+src\main.rs:

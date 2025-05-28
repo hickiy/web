a4308e62b7c06f720d13c0d344f8720b7b578b94
hickey <596310898@163.com>
2025-05-28 18:33:47 +0800

    formated some rust file

--- lang/rust/basic-syntax/eq.rs
+++ lang/rust/basic-syntax/eq.rs
@@ -1,11 +1,11 @@
 fn main() {
-  let a = f64::NAN;
-  let b = f64::NAN;
-  println!("{}", a == b); // false
-  println!("{:?}", a.partial_cmp(&b)); // None
+    let a = f64::NAN;
+    let b = f64::NAN;
+    println!("{}", a == b); // false
+    println!("{:?}", a.partial_cmp(&b)); // None
 
-  let c = 1;
-  let d = 2;
-  println!("{}", c == d); // false
-  println!("{:?}", c.partial_cmp(&d)); // Some(Less)
+    let c = 1;
+    let d = 2;
+    println!("{}", c == d); // false
+    println!("{:?}", c.partial_cmp(&d)); // Some(Less)
 }

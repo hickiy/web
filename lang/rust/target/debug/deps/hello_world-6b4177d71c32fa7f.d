c0f3b762c2eb72ebdfec98677e7a01df91f07bb4
hickey <596310898@163.com>
2025-05-28 13:44:41 +0800

    ignore the target from rust builder

--- lang/rust/target/debug/deps/hello_world-6b4177d71c32fa7f.d
+++
@@ -1,5 +0,0 @@
-E:\learn\web\lang\rust\target\debug\deps\libhello_world-6b4177d71c32fa7f.rmeta: src\main.rs
-
-E:\learn\web\lang\rust\target\debug\deps\hello_world-6b4177d71c32fa7f.d: src\main.rs
-
-src\main.rs:
--- lang/rust/target/debug/deps/hello_world-a9552440ff7b40e9.d
+++
@@ -1,5 +0,0 @@
-E:\learn\web\lang\rust\target\debug\deps\libhello_world-a9552440ff7b40e9.rmeta: src\main.rs
-
-E:\learn\web\lang\rust\target\debug\deps\hello_world-a9552440ff7b40e9.d: src\main.rs
-
-src\main.rs:

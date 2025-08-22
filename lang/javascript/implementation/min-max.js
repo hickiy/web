e5581d462793623affaec08ffbbafd655b1ad436
hickey <596310898@163.com>
2025-08-22 08:40:41 +0800

    lang: find maximum or minimum value in javascript

--- lang/javascript/implementation/min-max.js
+++ lang/javascript/implementation/min-max.js
@@ -1,4 +1,4 @@
-
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 /*找最大最小*/
 console.log(Math.max(...arr), Math.min(...arr)) // 9 1
-console.log(arr.sort((a, b) => a -b)[arr.length - 1], arr[0]) // 9 1
+console.log(arr.sort((a, b) => a - b)[arr.length - 1], arr[0]) // 9 1

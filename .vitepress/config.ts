8c199efe940f79cc050b96894418b519ca91d743
hickey <596310898@163.com>
2025-09-29 18:40:25 +0800

    update vitepress config file

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,38 +1,39 @@
 import { defineConfig } from 'vitepress';
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/web/',
   outDir: '.vitepress/dist',
   title: 'hickey blog',
-  head: [['link', { rel: 'icon', href: 'favicon.jpg' }]],
+  head: [['link', { rel: 'icon', href: '/web/favicon.jpg' }]],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    logo: 'favicon.jpg',
+    logo: '/web/favicon.jpg',
     siteTitle: 'Hickey',
     search: {
       provider: 'local'
     },
     nav: [
       { text: '首页', link: '/' },
-      { text: '部署', link: '/deploy/nginx/location.md', activeMatch: '/deploy/' },
-      { text: '框架', link: '/frame/vue2/init-instance.md', activeMatch: '/frame/' }
+      { text: '部署', link: '/doc/nginx/location.md', activeMatch: '/doc/nginx' },
+      { text: '框架', link: '/doc/vue2/init-instance.md', activeMatch: '/doc/vue2' }
     ],
     sidebar: {
-      '/deploy/': [
+      '/doc/nginx': [
         {
           text: 'nginx',
           items: [
-            { text: '路由优先级', link: '/deploy/nginx/location.md' },
-            { text: '代理示例', link: '/deploy/nginx/jsdelivr.md' }
+            { text: '路由优先级', link: '/doc/nginx/location.md' },
+            { text: '代理示例', link: '/doc/nginx/jsdelivr.md' }
           ]
         }
       ],
-      '/frame/': [
+      '/doc/vue2': [
         {
-          text: 'vue',
+          text: 'vue2',
           items: [
-            { text: '初始化实例', link: '/frame/vue2/init-instance.md' },
-            { text: '初始化状态', link: '/frame/vue2/init-state.md' },
-            { text: '语法风格指南', link: '/frame/vue2/vue.md' }
+            { text: '初始化实例', link: '/doc/vue2/init-instance.md' },
+            { text: '初始化状态', link: '/doc/vue2/init-state.md' },
+            { text: '语法风格指南', link: '/doc/vue2/vue.md' }
           ]
         }
       ]
